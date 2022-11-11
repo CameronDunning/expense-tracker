57ea1e9ef36fcfb0f46aa9af15999e35d51b4c32
Cameron <cdunning@avalanche.ca>
2022-11-10 19:07:51 -0800

    add register and logout functionality

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,34 @@
 import React from 'react'
 
+import { Navigate } from 'react-router-dom'
+import { Stack, Flex, Text, useToast } from '@chakra-ui/react'
+
+import { useUser } from '../Stores/UserStore'
+
 export const Home = () => {
-    return <div>Home</div>
+    const toast = useToast()
+
+    const user = useUser()
+
+    if (!user) {
+        toast({
+            title: 'You are not logged in.',
+            description: 'You have been redirected to the login page.',
+            status: 'error',
+            duration: 9000,
+            isClosable: true,
+        })
+        return <Navigate to={'/login'} />
+    }
+
+    return (
+        <Stack direction={'row'} flex={1} minH={'100%'}>
+            <Flex flex={{ base: 0, md: 1 }}>
+                <Text>Home</Text>
+                <Text>{user.email}</Text>
+                <Text>{user.firstName}</Text>
+                <Text>{user.lastName}</Text>
+            </Flex>
+        </Stack>
+    )
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,9 @@
+import { useState } from 'react'
+
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { Navigate } from 'react-router-dom'
 import {
     Button,
-    Checkbox,
     Flex,
     FormControl,
     FormLabel,
@@ -10,44 +13,125 @@ import {
     Image,
     Text,
     HStack,
+    Alert,
+    AlertIcon,
+    useToast,
 } from '@chakra-ui/react'
 import { GoogleButton } from '../components/GoogleButton/GoogleButton'
+
+import { auth } from '../config/firebase'
+import { useUser, useSetUser } from '../Stores/UserStore'
 import { Link } from '../components/Link/Link'
 
 export const Login = () => {
+    const toast = useToast()
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [errorMissingFields, setErrorMissingFields] = useState(false)
+    const [errorPassword, setErrorPassword] = useState(false)
+
+    const user = useUser()
+    const setUser = useSetUser()
+    if (user) {
+        return <Navigate to={'/'} />
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault()
+        setErrorMissingFields(false)
+        setErrorPassword(false)
+
+        if (!email || !password) {
+            setErrorMissingFields(true)
+            return
+        }
+
+        signInWithEmailAndPassword(auth, email, password)
+            .then(userCredential => {
+                // Signed in
+                const user = userCredential.user
+                console.log(user)
+                // ...
+                setUser(user)
+                toast({
+                    title: 'You are logged in.',
+                    status: 'success',
+                    duration: 9000,
+                })
+            })
+            .catch(error => {
+                const errorCode = error.code
+                const errorMessage = error.message
+                console.log(errorCode, errorMessage)
+                setErrorPassword(error.message)
+            })
+
+        // console.log('rememberMe', rememberMe)
+    }
+
     return (
-        <Stack direction={'row'} flex={1}>
+        <Stack direction={'row'} flex={1} minH={'100%'} style={styles.containerStack}>
             <Flex flex={{ base: 0, md: 1 }}>
                 <Image alt={'Login Image'} objectFit={'cover'} src={'/login.jpg'} />
             </Flex>
-            <Flex p={8} flex={1} align={'center'} justify={'center'}>
-                <Stack spacing={4} w={'full'} maxW={'md'}>
-                    <Heading fontSize={'2xl'}>Sign in to your account</Heading>
-                    <FormControl id="email">
-                        <FormLabel>Email address</FormLabel>
-                        <Input type="email" />
-                    </FormControl>
-                    <FormControl id="password">
-                        <FormLabel>Password</FormLabel>
-                        <Input type="password" />
-                    </FormControl>
-                    <Stack spacing={6}>
-                        <Stack direction={{ base: 'column', sm: 'row' }} align={'start'} justify={'space-between'}>
-                            <Checkbox>Remember me</Checkbox>
-                            <HStack>
-                                <Text>Don't have an account?</Text>
-                                <Link color={'blue.500'} href={'/register'}>
-                                    Register
-                                </Link>
-                            </HStack>
+            <form style={styles.form} onSubmit={handleSubmit}>
+                <Stack direction={'row'} flex={1} minH={'100%'}>
+                    <Flex p={8} flex={1} align={'center'} justify={'center'}>
+                        <Stack spacing={4} w={'full'} maxW={'md'}>
+                            <Heading fontSize={'2xl'}>Sign in to your account</Heading>
+                            <FormControl id="email" isRequired>
+                                <FormLabel>Email address</FormLabel>
+                                <Input type="email" onChange={e => setEmail(e.target.value)} />
+                            </FormControl>
+                            <FormControl id="password" isRequired>
+                                <FormLabel>Password</FormLabel>
+                                <Input type="password" onChange={e => setPassword(e.target.value)} />
+                            </FormControl>
+                            <Stack spacing={6}>
+                                <Stack
+                                    direction={{ base: 'column', sm: 'row' }}
+                                    align={'start'}
+                                    justify={'space-between'}>
+                                    {/* <Checkbox onChange={setRememberMe}>Remember me</Checkbox> */}
+                                    <HStack>
+                                        <Text>Don't have an account?</Text>
+                                        <Link color={'blue.500'} href={'/register'}>
+                                            Register
+                                        </Link>
+                                    </HStack>
+                                </Stack>
+                                <Button colorScheme={'blue'} variant={'solid'} onClick={handleSubmit} type={'submit'}>
+                                    Sign in
+                                </Button>
+                                {errorMissingFields && (
+                                    <Alert status="error">
+                                        <AlertIcon />
+                                        Please fill all fields
+                                    </Alert>
+                                )}
+                                {errorPassword && (
+                                    <Alert status="error">
+                                        <AlertIcon />
+                                        {errorPassword}
+                                    </Alert>
+                                )}
+                                <GoogleButton>Sign in with Google</GoogleButton>
+                            </Stack>
                         </Stack>
-                        <Button colorScheme={'blue'} variant={'solid'}>
-                            Sign in
-                        </Button>
-                        <GoogleButton>Sign in with Google</GoogleButton>
-                    </Stack>
+                    </Flex>
                 </Stack>
-            </Flex>
+            </form>
         </Stack>
     )
+}
+
+const styles = {
+    containerStack: {
+        marginTop: 0,
+    },
+    form: {
+        flex: 1,
+        margin: 0,
+    },
 }
